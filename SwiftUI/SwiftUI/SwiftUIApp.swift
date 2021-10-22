3c09d6320d9d9f0147eba37523bbf293610a3e97
diayan <diayans@outlook.com>
2021-10-22 15:00:43 +0000

    create swiftUI parent project

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by diayan siat on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
